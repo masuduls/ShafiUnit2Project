348606c38b4f7864a677aab62b1d84062015d1e4
1E10 Laptop <sample@bths.edu>
2022-10-17 10:55:43 -0400

    fix

---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LinearEquationRunner {
+    public static void main(String[] args) {
+
+        // initialization
+        Scanner scan = new Scanner(System.in);
+
+        // user input
+        System.out.print("Enter coordinate 1: ");
+        String cord1 = scan.nextLine();
+        int parse1 = Integer.parseInt(cord1);
+        System.out.print("Enter coordinate 2: ");
+        String cord2 = scan.nextLine();
+        int parse2 = Integer.parseInt(cord2);
+
+        // return
+        System.out.println("The two points are " + parse1 + " and " + parse2);
+        System.out.println("The two points are ");
+        System.out.println("The two points are ");
+        System.out.println("The two points are ");
+        System.out.println("The two points are ");
+
+        System.out.println("\nEnter a value for x: ");
+        System.out.println("\nThe point on the line is ");
+
+
+    }
+}
